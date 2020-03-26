3f21b3cf525b3b36ae1bf839c4b4b42097fc2121
sanskarp <sanskarpatro2012@gmail.com>
2020-03-27 00:54:10 +0530

    server

---
+++ controllers/image.js
@@ -0,0 +1,39 @@
+const clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+ apiKey: 'be8f713f69a64582a6ac55127b2082a4'
+});
+
+const handleApiCall = (req, res) => {
+	app.models
+	.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+	.then(data => {res.json(data)})
+	.catch(err => res.status(400).json('unable to work with api'))
+}
+
+
+const handleImageIp = (req, res, db) => {
+	const {id} = req.body;
+	// let found = false;
+	// database.users.forEach(user => {
+	// 	if(user.id === id){
+	// 		found = true;
+	// 		user.score++
+	// 		return res.json(user.score);
+	// 	}
+	// })
+	// if(!found){
+	// 	res.status(404).json('not found');
+	// }
+	db('users')
+	.where('id','=',id)
+	.increment('score',1)
+	.returning('score')
+	.then(score => res.json(score[0]))
+	.catch(err => res.status(400).json('cannot find user'))
+}
+
+module.exports = {
+	handleImageIp:handleImageIp,
+	handleApiCall:handleApiCall
+}
---
+++ server.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+const register = require('./controllers/register.js');
+const image = require('./controllers/image.js');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : 'test',
+    database : 'brainybrain'
+  }
+});
+
+// db.select('name', 'email', 'joined').from('users').then(data => console.log(data)); 
+
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+	res.send(database.users);
+})
+
+app.post('/signin', (req, res) => {
+	const {pass, email} = req.body;
+	if(!pass || !email){
+		return res.status(400).json('incorrect form submission');
+	}
+
+	db.select('email', 'hash').from('login')
+	.where('email', '=', email)
+	.then(data =>{
+		const isValid = bcrypt.compareSync(pass, data[0].hash);
+		if(isValid){
+			return db.select('*').from('users')
+			.where('email', '=', email)
+			.then(user => res.json(user[0]))
+			.catch(err => res.status(400).json('user not found'))
+		} else{
+			res.status(400).json('wrong credentials')
+		}
+	})
+	.catch(err => res.status(400).json('wrong credentials'))
+})
+
+//advice for bigger apps. put each component in different .js file. Here, I am doing
+//this for the register component for future sanskar's reference.
+app.post('/register', (req,res) => {register.handleRegister(req,res,db,bcrypt)})
+
+app.get('/profile/:id', (req, res) => {
+	const {id} = req.params;
+	db.select('*')
+		.from('users')
+		.where({id: id})
+		.then(user => { 
+			if(user.length){
+				res.json(user[0])
+			}else{
+				res.status(400).json('not found');
+			}
+		})
+		.catch(err => res.status(400).json('error gettign user'));
+})
+
+app.put('/image', (req, res) => {image.handleImageIp(req,res,db)})
+app.post('/imageUrl', (req, res) => {image.handleApiCall(req,res,db)})
+
+app.listen(3333, ()=>{
+	console.log('app running on port 3333');
+});
